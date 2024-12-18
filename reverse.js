3915dd9d866efff58fb59e3b913533ad88bb80ec
Jordi Martínez <jmartinezg.fp@gmail.com>
2024-12-18 08:58:05 +0100

    Documentation updated

--- reverse.js
+++ reverse.js
@@ -4,46 +4,58 @@ const ExcelJs = require('exceljs');
 const inquirer = require('inquirer');
 require("colors");
 
-const resultsDir = './results';
+const resultsDir = './results'; // Directory to store the results
+const excelDir = './excels'; // Directory where the excel files are stored
 
-const getExcelFiles = async (excelDir, extension = null) => {
+/**
+ * Get all excel files in the directory defined by excelDir
+ * @param extension
+ * @return {Promise<*[]>}
+ */
+const getExcelFiles = async (extension = null) => {
     try {
-        const files = await fs.readdir(excelDir);
+        const files = await fs.readdir(excelDir); // Get all the files in the directory
 
         const excelFiles = [];
         for (const file of files) {
-            const filePath = path.join(excelDir, file); // Obtener la ruta completa
-            const stats = await fs.stat(filePath); // Obtener información del fichero
+            const filePath = path.join(excelDir, file);
+            const stats = await fs.stat(filePath);
 
-            if (stats.isFile()) { // Comprobar si es un fichero
-                if (extension) { // Si se especificó una extensión
+            // If it's a file and has the specified extension add it to the list
+            if (stats.isFile()) {
+                if (extension) {
                     if (path.extname(file).toLowerCase() === extension.toLowerCase()) {
                         excelFiles.push(file);
                     }
                 } else {
-                    excelFiles.push(file); // Si no hay extensión, añadir todos los ficheros
+                    excelFiles.push(file);
                 }
             }
         }
         return excelFiles;
     } catch (error) {
-        console.error(`Error al leer el directorio ${excelDir}:`, error);
-        return []; // Devolver un array vacío en caso de error
+        console.error(`Error reading directory ${excelDir}:`, error);
+        return [];
     }
 };
 
+/**
+ * Read a file as an ArrayBuffer
+ * @param filePath
+ * @return {Promise<ArrayBuffer|null>}
+ */
 async function readFileAsArrayBuffer(filePath) {
     try {
         const fileBuffer = await fs.readFile(filePath);
-        return fileBuffer.buffer; // Obtener el ArrayBuffer del Buffer
+        return fileBuffer.buffer;
     } catch (error) {
-        console.error(`Error al leer el archivo ${filePath}:`, error);
+        console.error(`Error reading file ${filePath}:`, error);
         return null;
     }
 }
 
 /**
- * Ask the user for a string
+ * Ask the user for a number
  * @param message {string}
  * @returns {Promise<number>}
  */
@@ -59,14 +71,24 @@ const askNumber = async (message) => {
     return Number(number);
 }
 
-
+/**
+ * Process all the worksheets in the given excel file and write the translations on i18n-json files in the results directory
+ * @param filePath
+ * @param fileName
+ * @return {Promise<void>}
+ */
 const processExcelFile = async (filePath, fileName) => {
     try {
+        // Ask the user for the number of languages
         const numberOfLanguages = await askNumber("How many languages do you have?");
+
         readFileAsArrayBuffer(filePath).then(async (buffer) => {
             try {
+                // Load the excel file as a workbook
                 const workbook = new ExcelJs.Workbook();
                 const wbFile = await workbook.xlsx.load(buffer);
+
+                // Get all the worksheets in the workbook
                 const worksheets = wbFile.worksheets;
 
                 if (worksheets.length === 0) throw new Error("No sheet found");
@@ -74,11 +96,14 @@ const processExcelFile = async (filePath, fileName) => {
                 const worksheetTranslations = {};
                 for (const worksheet of worksheets) {
                     console.log(`Processing sheet ${worksheet.name}...`);
-                    const languages = [];
-                    const translations = {};
-                    worksheet.eachRow((row, rowNumber) => {
-                        const key = row.getCell(1).value?.toString() || "";
+                    const languages = []; // Array to store the languages
+                    const translations = {}; // Object to store the translations
 
+                    // Iterate over each row in the worksheet
+                    worksheet.eachRow((row, rowNumber) => {
+                        const key = row.getCell(1).value?.toString() || ""; // Get the key from the first column
+
+                        // If the row number is 1, get the number of languages and initialize the translations object
                         if (rowNumber === 1) {
                             for (let i = 2; i <= numberOfLanguages + 1; i++) {
                                 const cell = row.getCell(i);
@@ -90,6 +115,7 @@ const processExcelFile = async (filePath, fileName) => {
                             return;
                         }
 
+                        // If the key is not empty, iterate over each language and add the translations to the translations object
                         if (key) {
                             for (let i = 2; i <= numberOfLanguages + 1; i++) {
                                 const cell = row.getCell(i);
@@ -117,8 +143,10 @@ const processExcelFile = async (filePath, fileName) => {
                             }
                         }
                     });
+                    // Add the translations object to the worksheet attribute of the worksheetTranslations object
                     worksheetTranslations[worksheet.name] = translations;
 
+                    // Create the results directories if they don't exist
                     const resultsFiles = await fs.readdir(resultsDir);
                     if (!resultsFiles.includes(fileName)) {
                         await fs.mkdir(path.join(resultsDir, fileName));
@@ -127,47 +155,60 @@ const processExcelFile = async (filePath, fileName) => {
                         await fs.mkdir(path.join(resultsDir, fileName));
                     }
 
+                    // Get the path to the results directory
                     const resultDirPath = path.join(resultsDir, fileName);
 
+                    // Create the languages directories in the results directory
                     for (const language of languages) {
                         await fs.mkdir(path.join(resultDirPath, language));
                     }
 
+                    // Get the list of files to write
                     const filesToWrite = Object.keys(worksheetTranslations);
+
+                    // Iterate over the files to write
                     for (const file of filesToWrite) {
-                        const fileTranslations = worksheetTranslations[file];
+                        const fileTranslations = worksheetTranslations[file]; // Get the translations for the current file
+
+                        // Iterate over the languages
                         Object.entries(fileTranslations).forEach(([language, translations]) => {
+                            // Get the path to the file
                             const filePath = path.join(resultDirPath, language, `${file}.json`);
 
+                            // Sort the keys alphabetically
                             const translationsKeys = Object.keys(translations).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
                             const translationsSorted = {};
                             for (const key of translationsKeys) {
                                 translationsSorted[key] = translations[key];
                             }
 
-                            const fileTranslationsAsString = JSON.stringify(translationsSorted, null, 2);
+                            // Convert the object to a string and write it to the file
+                            const fileTranslationsAsString = JSON.stringify(translationsSorted, null, 4);
                             fs.writeFile(filePath, fileTranslationsAsString);
                         });
                     }
                 }
             } catch (error) {
-                console.error(`Error al procesar el fichero ${filePath}:`, error);
+                console.error(`Error processing file ${filePath}:`, error);
             }
         });
     } catch (error) {
-        console.error(`Error al leer el fichero ${filePath}:`, error);
+        console.error(`Error reading file ${filePath}:`, error);
     }
 };
 
+/**
+ * Start the conversion process from the excel translations to i18n-json files
+ * @return {Promise<void>}
+ */
 const startConversion = async () => {
-    const excelDir = './excels';
     try {
-        const xlsxFiles = await getExcelFiles(excelDir, '.xlsx');
+        const xlsxFiles = await getExcelFiles('.xlsx');
         for (const file of xlsxFiles) {
             await processExcelFile(path.join(excelDir, file), file);
         }
     } catch (error) {
-        console.error("Error en startConversion:", error)
+        console.error("Error starting the conversion process:", error)
     }
 };
 
